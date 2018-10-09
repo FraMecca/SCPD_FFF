61e8aba0fd26b5d3b0738e0b789a846402abcc42
GallaFrancesco <francesco.galla3@gmail.com>
2018-10-09 18:53:00 +0200

    boost mpi

--- mpi/mpi_main.cpp
+++ mpi/mpi_main.cpp
@@ -1,31 +1,34 @@
 #include <assert.h>
 #include <iostream>
 #include <tuple>
-#include <mpi.h>
+//#include <mpi.h>
 #include "settings.hpp"
 #include "libcgl.hpp"
 #include "libga.hpp"
+#include <boost/mpi/environment.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/collectives.hpp>
+#include <boost/serialization/string.hpp>
 
-using namespace std;
+namespace mpi = boost::mpi;
 
-static int world_size = 0;
 
-int next_rank(size_t i)
+int next_rank(size_t i, mpi::communicator& world)
 {
-    assert(world_size > 0);
-    return i % (world_size - 1) + 1;
+    assert(world.size() > 0);
+    return i % (world.size() - 1) + 1;
 }
 
-void send_grid(Cgl<DIM> c, int index)
+void send_grid(Cgl<DIM> c, int index, mpi::communicator& world)
 {
     auto gene = c.getGene()->to_string();
-    MPI_Send(gene.c_str(), gene.size(), MPI_CHAR, next_rank(index), 0, MPI_COMM_WORLD);
+	world.send(next_rank(index, world), 0, gene);
 }
 
-//tuple<std::vector<Cgl<DIM>>, std::vector<MPI_Request*>> first_generation()
-std::vector<Cgl<DIM>> first_generation()
+std::vector<Cgl<DIM>> first_generation(mpi::communicator& world)
 {
     auto people = std::vector<Cgl<DIM>>();
+	auto grids = std::vector<std::string>();
 
     for(size_t i = 0; i < POPSIZE; ++i){
         auto c = Cgl<DIM>(SIDE,N_ITERATIONS);
@@ -33,43 +36,45 @@ std::vector<Cgl<DIM>> first_generation()
         c.max_iteration = N_ITERATIONS;
         c.prepareGrid();
         people.push_back(c);
-        // SEND TO SLAVES here
-        send_grid(c, i);
+		grids.push_back(c.getGene()->to_string());
     }
+	auto outg = std::vector<std::string>(people.size(),"");
+	cout << "scatter" << endl;
+	cout << world.size() << endl;
+	auto sizes = std::vector<int>(world.size(), 1);
+	cout << sizes[3] << endl;
+	//sizes[0] = 0;
+	mpi::scatterv(world, &grids, sizes, &outg, 0);
+	cout << "scatter_" << endl;
 	cout << "master: sent first" << endl;
-	MPI_Barrier(MPI_COMM_WORLD);
 
     assert(people.size() == POPSIZE);
 	return people;
 }
 
-std::vector<double> recv_fitness()
+std::vector<double> recv_fitness(mpi::communicator& world)
 {
-	// len = popsize, init a -1 tutti i campi
     auto fitness = std::vector<double>(POPSIZE, -1);
-    //auto requests = std::vector<MPI_Request>(POPSIZE);
-	MPI_Request requests[POPSIZE];
 
     for(int i=0; i<POPSIZE; ++i) {
-        //MPI_Irecv(&fitness[i], sizeof(double), MPI_DOUBLE, next_rank(i), 0, MPI_COMM_WORLD, &requests[i]);
-        MPI_Recv(&fitness[i], sizeof(double), MPI_DOUBLE, next_rank(i), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		// TODO gather
+		world.recv(next_rank(i,world), 0, fitness[i]);
     }
-	MPI_Barrier(MPI_COMM_WORLD);
+	world.barrier();
 	cout << "master: received" << endl;
 
-    //MPI_Request* vr = &requests[0]; // from vector to array
-    //MPI_Waitall(POPSIZE, requests, MPI_STATUSES_IGNORE);
     return fitness;
 }
 
-void master(int rank)
+void master(mpi::communicator& world)
 {
     //auto tup = first_generation();
     //auto people = std::get<0>(tup);
-    auto people = first_generation();
+    auto people = first_generation(world);
+	auto rank = world.rank();
 
     for(size_t g = 0; g < N_GENERATIONS; ++g){
-        auto fitness = recv_fitness();
+        auto fitness = recv_fitness(world);
         // update population vector with fitness
         for (int i; i<POPSIZE; i++) {
             people[i].fitness = fitness[i];
@@ -87,20 +92,21 @@ void master(int rank)
 			// free and reassign
 			people[i].release();
 			people[i] = c;
-			//MPI_Request* req = (MPI_Request*)malloc(sizeof(MPI_Request));
 			// SEND TO SLAVES here
-			send_grid(c, i);
+			send_grid(c, i, world);
 		}
-		MPI_Barrier(MPI_COMM_WORLD);
+		world.barrier();
 		cout << rank << ": sent" << endl;
     }
     string end = "end";
-    MPI_Bcast((void*)end.c_str(), end.size(), MPI_CHAR, 0, MPI_COMM_WORLD);
+	mpi::broadcast(world, end, rank);
 }
 
 
-void routine(int rank)
+void routine(mpi::communicator& world)
 {
+	auto rank = world.rank();
+
     // create target
     vector<double> target = vector<double>(DIM*DIM/(SIDE*SIDE)); //DIM*DIM / side^2;
     for(size_t i = 0; i < target.size()/2; ++i){
@@ -111,26 +117,28 @@ void routine(int rank)
     }
 
     if(rank == 0){
-        master(rank);
+        master(world);
         return;
     }
     // slave here
 
-    char buf[DIM*DIM];
-
     while(true) {
-        MPI_Recv(buf, DIM*DIM, MPI_CHAR, 0, 0, MPI_COMM_WORLD, NULL);
-		MPI_Barrier(MPI_COMM_WORLD);
+		//string buf;
+		auto buf = std::vector<std::string>();
+		// recv from master
+		mpi::scatterv(world, &buf, 1, 0);
+		cout << buf[rank] << endl;
+
 		cout << rank << ": received" << endl;
-        string stbuf = string(buf);
-        if (stbuf == "end") {
+        if (buf[rank] == "end") {
             return;
         }
-        auto person = Cgl<DIM>(stbuf, SIDE, N_ITERATIONS);
+        auto person = Cgl<DIM>(buf[rank], SIDE, N_ITERATIONS);
         assert(person.max_iteration > 0);
         person.GameAndFitness(target);
-        MPI_Send(&person.fitness, sizeof(person.fitness), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-		MPI_Barrier(MPI_COMM_WORLD);
+		// send to master
+        world.send(0, 0, person.fitness);
+		world.barrier();
     }
 }
 
@@ -142,12 +150,10 @@ main(int argc, char* argv[])
 {
     int rank, poolSize;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	mpi::environment env(argc, argv);
+	mpi::communicator world;
 
-    routine(rank);
+    routine(world);
 
-    MPI_Finalize();
     return 0;
 }
